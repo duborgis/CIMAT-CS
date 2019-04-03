f99bc11707023dacc2359732e9cd05d14c748291
DuBorgi <duborgis@gmail.com>
2019-04-03 12:33:13 -0300

    CHANGES

---
+++ commit2.bat
@@ -0,0 +1,5 @@
+cd C:/Users/OdnSpceMaker/Documents/GitHub/CIMAT-CS
+git remote set-url origin https://github.com/duborgis/CIMAT-CS.git
+git commit -am "autosave %date%-%time:~0,8%"
+git push origin master
+exit
